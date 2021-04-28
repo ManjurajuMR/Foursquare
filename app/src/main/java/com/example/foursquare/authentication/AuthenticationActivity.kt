6e5e6ea59d0db5572691657a12412b4f4d44d8d3
Manjuraju <manjurajumr5@gmail.com>
2021-04-28 18:33:14 +0530

    first

---
+++ app/src/main/java/com/example/foursquare/authentication/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foursquare.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.foursquare.R
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
