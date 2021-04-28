56c8ea8c8e657436bbcfb7933bf9190ebad0bf6d
Sahukarvaibhavi <vaibhavisahukar11@gmail.com>
2021-04-28 19:27:20 +0530

    comment

--- app/src/main/java/com/example/foursquare/authentication/ForgotpasswordFragment.kt
+++ app/src/main/java/com/example/foursquare/authentication/ForgotpasswordFragment.kt
@@ -1,25 +1,22 @@
-package com.example.foursquare
+package com.example.foursquare.authentication
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.foursquare.R
 
-class signup : Fragment() {
+class ForgotpasswordFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
+    //oncreateview
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        return inflater.inflate(R.layout.fragment_forgotpassword, container, false)
     }
 
 }
--- app/src/main/java/com/example/foursquare/authentication/Signup.kt
+++ app/src/main/java/com/example/foursquare/authentication/Signup.kt
@@ -1,21 +1,21 @@
-package com.example.foursquare
+package com.example.foursquare.authentication
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.foursquare.R
 
-class ForgotpasswordFragment : Fragment() {
+class Signup : Fragment() {
 
-    //oncreateview
+    //signup fragment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgotpassword, container, false)
+        return inflater.inflate(R.layout.fragment_signup, container, false)
     }
-
 }
