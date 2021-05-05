de117dfafc11c9891ab6228aed18cc77aed4b85c
Manjuraju <manjurajumr5@gmail.com>
2021-05-05 17:04:19 +0530

    content_main

--- app/src/main/java/com/example/foursquare/MainActivity.kt
+++ app/src/main/java/com/example/foursquare/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.widget.TextView
+import com.example.foursquare.Home.HomeActivity
 
 class MainActivity : AppCompatActivity() {
 
