500a5fa832e09a235007d697f310052e8ffb91df
Manish C M <manishcm1998@gmail.com>
2021-05-12 10:41:28 +0530

    det activity

--- app/src/main/java/com/example/foursquare/DetailsScreenActivity.kt
+++ app/src/main/java/com/example/foursquare/DetailsScreenActivity.kt
@@ -7,22 +7,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.example.foursquare.repository.HomeScreenRepository
+import com.example.foursquare.viewmodel.AuthenticationViewModel
+import com.example.foursquare.viewmodel.HomeViewModel
 import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_details_screen.*
 
 class DetailsScreenActivity : AppCompatActivity() {
     lateinit var myDialog : Dialog
+    private lateinit var homeViewModel : HomeViewModel
+    //private lateinit var repmodel : HomeScreenRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_details_screen)
         supportActionBar?.hide()
+        homeViewModel = ViewModelProvider.AndroidViewModelFactory(application).create(HomeViewModel::class.java)
 
         val detscreen_tb=findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar1)
-        detscreen_tb.setNavigationOnClickListener {
+        /*detscreen_tb.setNavigationOnClickListener {
             onBackPressed()
+        }*/
+        myDialog = Dialog(this)
+        homeViewModel.getPlaceDetailsByPlaceId(10)
+        setlivedata()
+
+        add_review.setOnClickListener {
+            Toast.makeText(this,"hii",Toast.LENGTH_LONG).show()
         }
 
-        setContentView(R.layout.activity_details_screen)
-        myDialog = Dialog(this)
     }
 
     fun ShowPopup(v : View?){
@@ -35,6 +49,43 @@ class DetailsScreenActivity : AppCompatActivity() {
         })
         //myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         myDialog.show()
+    }
+
+    fun setlivedata(){
+        homeViewModel.homeRepository.resPlace.observe(this,{
+            val place_name = it.data.name
+            val place_image = it.data.image
+            val place_type = it.data.placeType[0].name
+            val overall_rating = it.data.overallRating
+            val overview = it.data.overview
+            val address = it.data.address
+            val phone_num = it.data.phone
+            val latitude = it.data.latitude
+            val longitude = it.data.longitude
+
+            setDataToLayout(place_name,place_image,place_type,overall_rating,overview,address,phone_num,latitude,longitude)
+        })
+        /*homeViewModel.getPlaceDetailsByPlaceId(10).observe(this,{
+        })*/
+    }
+
+    fun setDataToLayout(
+        place_name: String,
+        place_image: String,
+        place_type: String,
+        overall_rating: Double,
+        overview: String,
+        address: String,
+        phone_num: Long,
+        latitude: Double,
+        longitude: Double
+    ) {
+        detscreentoolbar_title.setText(place_name)
+        res_type.setText(place_type)
+        res_overview.setText(overview)
+        res_address.setText(address)
+        res_phno.setText(phone_num.toString())
+
     }
 
 }
---
+++ app/src/main/java/com/example/foursquare/services/HomescreenApi.kt
@@ -0,0 +1,16 @@
+package com.example.foursquare.services
+
+import com.example.foursquare.model.Place
+import com.example.foursquare.model.User
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface HomescreenApi {
+
+    @GET("PlaceApi/placeById?")
+    fun getPlaceDetailsByPlaceId(
+        @Query("placeId") placeId: Int?
+    ) : Call<Place>
+
+}
